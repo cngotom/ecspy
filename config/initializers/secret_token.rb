f459d877e0f2ea362a063eca341101808c2abf8b
gotom <liuxingfeiyu@gmail.com>
2014-01-16 21:59:17 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecspy::Application.config.secret_token = '9efc7c3c7bd37ffe2690bf0361bf78b117558aa5264e8f518be07a4d724900acb540008be5010ef950b9d696cc27988066a98eb93f77aa40a199c8ead528ab3f'
